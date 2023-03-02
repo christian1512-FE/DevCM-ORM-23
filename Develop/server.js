062bba0d0f09ec5a816535f90fd7234f99533262
Christian Mendez <christianmendez@Christians-MBP.attlocal.net>
2023-03-02 09:22:13 -0600

    models done

--- Develop/server.js
+++ Develop/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
 // import sequelize connection
-// ADDED LINE 5
 const sequelize = require('./config/connection');
 
 // iMPORT MODEL TO SYNC TABLE WITH DATABASE
@@ -15,8 +14,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-
-// sequelize.sync().then(() => { 
-app.listen(PORT, () => {
+// Force true to drop/recreate table(s) on every sync
+sequelize.sync({ force: true }).then(() => {
+  app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}!`);
+  });
 });
